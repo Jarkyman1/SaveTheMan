043e1ffe45a558f55b6540f9f1455f885c8339ef
Jackie H. Jensen <jackiejensen@2.42.16.172.rev.sfr.net>
2017-01-03 17:45:56 +0100

    Initial Commit

---
+++ SaveTheMan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SaveTheMan
+//
+//  Created by Jackie H. Jensen on 03/01/2017.
+//  Copyright © 2017 JHJService. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
